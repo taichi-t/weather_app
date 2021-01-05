5a814575849806745f603f51d6be9c2ee0ab18f6
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2021-01-04 18:25:21 -0800

    :recycle: Refactor.

--- webpack.config.ts
+++ webpack.config.ts
@@ -110,7 +110,20 @@ const clientConfig: Configuration = {
     new MiniCssExtractPlugin({
       filename: 'style.css',
     }),
-    new HtmlWebpackPlugin({ template: './src/index.html' }),
+    new HtmlWebpackPlugin({
+      templateContent: `
+        <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Weather App</title>
+        </head>
+        <body>
+          <div id="root"></div>
+        </body>
+        </html>
+    `,
+    }),
   ],
   devtool: 'inline-source-map',
 };
