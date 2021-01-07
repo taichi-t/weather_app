91de3d478906136e03a43d51a6559c525172270a
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2021-01-06 16:55:55 -0800

    :boom: Remove react-dom.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,15 @@
 import * as React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Layout from '@/components/Layout/Layout';
 import { useSelector } from 'react-redux';
 import { uiSelector } from '@/features/ui/slice';
 import '@/style/index.css';
 import Home from '@/components/Home/Home';
-
 import { Helmet } from 'react-helmet';
 
 const App: React.FC = () => {
   const { ui } = useSelector(uiSelector);
   return (
-    <BrowserRouter>
+    <>
       <Helmet>
         <style>{`body { background-color: ${
           ui.theme === 'theme-light' ? '#f2f3f5' : '#181818'
@@ -21,12 +19,10 @@ const App: React.FC = () => {
       </Helmet>
       <div className={`${ui.theme} bg-background content-transition`}>
         <Layout>
-          <Switch>
-            <Route path="/" exact component={Home} />
-          </Switch>
+          <Home />
         </Layout>
       </div>
-    </BrowserRouter>
+    </>
   );
 };
 
